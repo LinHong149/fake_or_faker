d05b3d1a3afcc75896ac9b24aba8cef046b0e969
LinHong <linlin.hong1@gmail.com>
2025-05-13 10:46:20 -0400

    Headline class

--- src/Headline.java
+++ src/Headline.java
@@ -7,6 +7,56 @@
  *
  * @author linhong
  */
-public class Headline {
+
+import java.util.*;
+import java.io.*;
     
+public class Headline {
+    private String text;
+    private String explanation;
+    private boolean isTrue;
+    private int index;
+    public static ArrayList<Integer> visited = new ArrayList<>();
+    private static final String fileName = "database.txt";
+    
+
+    public Headline() {
+        // rng the index
+        
+        this.index = index;
+        
+        try{
+            Scanner file = new Scanner(new File(fileName));
+            int count = 1;
+            while (file.hasNext()){
+                String[] line = file.nextLine().split(",");
+                if (index == count){
+                    this.text = line[0];
+                    this.isTrue = Boolean.parseBoolean(line[1]);
+                    this.explanation = line[2];
+                }
+                count++;
+            }
+        } catch(FileNotFoundException e){
+            System.out.println("Error: "+e);
+        }
+        
+        
+    }
+
+    public String getText() { 
+        return text; 
+    }
+    
+    public String getExplanation() { 
+        return explanation; 
+    }
+    
+    public boolean getIsReal(){
+        return isTrue;
+    }
+    
+    public static ArrayList<Integer> getVisited(){
+        return visited;
+    }
 }
